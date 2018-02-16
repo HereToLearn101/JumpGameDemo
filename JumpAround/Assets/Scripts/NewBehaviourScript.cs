f5e62f6d20582873f3543cb3a5c1d7a99ff700b8
Edgar Moso <tedismoso02@gmail.com>
2018-02-16 18:35:45 -0500

    Added new changes to the JumpGameDemo: 1) Made changes so the player jump once per key press by adding Input.GetKeyDown('space')   2) Added an if-else statement that respawns the player object on left if he has not jumped the obstacle. If he has, respawn on the right.

--- JumpAround/Assets/Scripts/NewBehaviourScript.cs
+++ JumpAround/Assets/Scripts/NewBehaviourScript.cs
@@ -25,10 +25,45 @@ public class NewBehaviourScript : MonoBehaviour
 		// Storing the Rigidbody 2D component in a variable.
 		// Used GetComponent to assign it with the physics component (Rigidbody 2D).
 		// Note that GetComponent takes in a generic, which in this case, is a type Rigidbody2D object.
-		var rigidBody = GetComponent<Rigidbody2D> ();
+		var rigidBody = GetComponent<Rigidbody2D>();
+        var transform = GetComponent<Transform>();
+
+        // This is to create an action to move the object to the right by pressing the right arrow key.
 		if (Input.GetKey("right")) {
-			// Debug.Log () prints a message on the console at Unity.
-			Debug.Log("The right arrow was pressed!");
+            // Assigning a new velocity to rigidBody variable by using a structure called Vector2.
+            // First parameter is the vector's x value, second parameter is the vector's y value.
+            rigidBody.velocity = new Vector2(5, 0);
 		}
+
+        // This is to create an action to move the object to the left by pressing the left arrow key.
+        if (Input.GetKey("left"))
+        {
+            rigidBody.velocity = new Vector2(-5, 0);
+            //rigidBody.velocity.x
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            rigidBody.velocity = new Vector2(0, 10);
+        }
+
+        if (transform.position.y < -6)
+        {
+            if (transform.position.x < 2)
+            {
+                transform.position = new Vector2(-10, 0);
+            } else
+            {
+                transform.position = new Vector2(10, 0);
+            }
+            
+        }
+
+        /*
+        if (transform.position.x > 2)
+        {
+            transform.position = new Vector2(10, 1);
+        }
+        */
 	}
 }
